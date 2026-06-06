311e1337fddb48644994d804418de322698bef3d
matildalikho-cpu <matildalikho@gmail.com>
2026-06-06 15:35:44 +0500

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -4,13 +4,12 @@ import { routeTree } from "./routeTree.gen";
 
 export const getRouter = () => {
   const queryClient = new QueryClient();
-
   const router = createRouter({
     routeTree,
     context: { queryClient },
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
+    history: undefined,
   });
-
   return router;
 };
